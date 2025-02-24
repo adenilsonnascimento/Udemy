3eb9d3b5245dd9037c01c45722f3c57cb3c3a5f6
Adenilson Nascimento <147216196+adenilsonnascimento@users.noreply.github.com>
2025-02-24 11:01:13 -0300

    Update oo_pilar_encapsulamento.php

--- htdocs/php_orientacao_a_objetos/380 OO - Pilar do Encapsulamento parte 1/oo_pilar_encapsulamento.php
+++ htdocs/php_orientacao_a_objetos/380 OO - Pilar do Encapsulamento parte 1/oo_pilar_encapsulamento.php
@@ -2,48 +2,119 @@
 // Definição da classe Pai
 class Pai
 {
-	private $nome = 'Jorge'; // Atributo privado, acessível apenas dentro da classe
-	protected $sobrenome = 'Silva'; // Atributo protegido, acessível na classe e em subclasses
-	public $humor = 'Feliz'; // Atributo público, acessível de qualquer lugar
+    //private, protected, public são operadores de visibilidade e servem para controlar o acesso aos atributos
 
-	// Método mágico __get para acessar atributos inacessíveis ou privados/protected
-	public function __get($attr)
-	{
-		return $this->$attr; // Retorna o valor do atributo especificado
-	}
+    private $nome = 'Adenilson';
+    protected $sobrenome = 'Nascimento';
+    public $humor = 'Feliz';
 
-	// Método mágico __set para definir valores em atributos inacessíveis ou privados/protected
-	public function __set($attr, $value)
-	{
-		$this->$attr = $value; // Define o valor do atributo especificado
-	}
+    public function getNome()
+    {
+        return $this->nome;
+    }
 
-	// Método privado, acessível apenas dentro da classe
-	private function executarMania()
-	{
-		echo 'Assobiar'; // Exibe uma mensagem indicando a mania
-	}
+    public function setNome($value)
+    {
+        if (strlen($value) >= 3) {
+            $this->nome = $value;
+        }
+    }
 
-	// Método protegido, acessível na classe e em subclasses
-	protected function responder()
-	{
-		echo 'Oi'; // Exibe uma mensagem indicando a resposta
-	}
+    public function getSobrenome()
+    {
+        return $this->sobrenome;
+    }
 
-	// Método público para executar uma ação aleatória
-	public function executarAcao()
-	{
-		$x = rand(1, 10); // Gera um número aleatório entre 1 e 10
-		if ($x >= 1 && $x <= 8) { // Se o número estiver entre 1 e 8
-			$this->responder(); // Chama o método protegido responder()
-		} else { // Caso contrário (números 9 ou 10)
-			$this->executarMania(); // Chama o método privado executarMania()
-		}
-	}
+    public function setSobrenome($value)
+    {
+        if (strlen($value) >= 3) {
+            $this->sobrenome = $value;
+        }
+    }
+
+    // Métodos mágicos que fazem o mesmo que os métodos acima
+    public function __get($atributo)
+    {
+        if (property_exists($this, $atributo)) {
+            return $this->$atributo;
+        }
+    }
+
+    public function __set($atributo, $value)
+    {
+        if (property_exists($this, $atributo)) {
+            $this->$atributo = $value;
+        }
+    }
+
+    // Em funções, precisamos atribuir valor das funções private e protected dentro de uma função pública para mostrá-las
+    public function executarMania()
+    {
+        echo 'Programar';
+        echo '<br/>';
+        $this->executarTristeza();
+        echo '<br/>';
+        $this->executarFeliz();
+    }
+
+    private function executarTristeza()
+    {
+        echo 'Programar em Java';
+    }
+
+    protected function executarFeliz()
+    {
+        echo 'Programar em PHP';
+    }
+
+    // Teste com função
+    public function gostarDeProgramar($programar)
+    {
+        $gosto = $programar;
+        if ($gosto == 'programar') {
+            $this->executarFeliz();
+        } else {
+            $this->executarTristeza();
+        }
+    }
 }
 
-// Criação de uma instância da classe Pai
 $pai = new Pai();
+echo $pai->getNome(); // Exibe "Adenilson"
 
-// Chamada do método executarAcao()
-echo $pai->executarAcao();
+echo '<br/>';
+
+$pai->setNome('Ferreira');
+echo $pai->getNome(); // Exibe "Ferreira"
+
+echo '<br/>';
+
+echo $pai->getSobrenome(); // Usando o método getSobrenome() para acessar o atributo protegido
+
+echo '<br/>';
+
+$pai->setSobrenome('Marinos');
+echo $pai->getSobrenome(); // Usando o método getSobrenome() para acessar o atributo protegido após a alteração
+
+// Utiliza o método mágico para fazer o mesmo que acima, otimizando a sintaxe
+echo '<br/> ______________________________________<br/>';
+echo $pai->__get('nome');
+echo '<br/>';
+echo $pai->__get('sobrenome');
+
+// Com os métodos mágicos get e set, conseguimos atribuir valor diretamente
+echo '<br/>';
+$pai->__set('nome', 'Adenilson');
+echo $pai->__get('nome');
+echo '<br/>';
+$pai->__set('sobrenome', 'Nascimento');
+echo $pai->__get('sobrenome');
+
+// Métodos protegidos e privados não podem ser acessados diretamente fora da classe
+echo '<br/> ______________________________________<br/>';
+echo $pai->executarMania();
+echo '<br/> ______________________________________<br/>';
+
+// Testando a função gostarDeProgramar
+$pai->gostarDeProgramar('programar');
+?>
